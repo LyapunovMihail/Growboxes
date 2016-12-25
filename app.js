e9183f23137281d828a51d96d1fd52cdeef66136
Lyapunov Mihail <LyapunovMihail@gmail.com>
2016-12-25 21:53:35 +0300

    'test'

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var config = require('./config');
+var mongoose = require('./libs/mongoose');
+var HttpError = require('./error/index').HttpError;
 
 var app = express();
 app.set('port', config.get('port'));
@@ -15,47 +17,57 @@ app.engine('ejs', require('ejs-locals'));
 app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
 
-
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.cookieParser());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res, next) {
-	res.render('index', {title: '<b>HELL</b>'});
-});
+var sessionStore = require('./libs/sessionStore');
+
+app.use(express.session({
+	secret: config.get('session:secret'),
+	key: config.get('session:key'),
+	cookie: config.get('session:cookie'),
+	store: sessionStore
+}));
+/*app.use(function(req, res, next) {
+	req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+	res.send("Visits: " + req.session.numberOfVisits);
+});*/
+
+app.use(require('./middleware/sendHttpError'));
+app.use(require('./middleware/loadUser'));
+
+app.use(app.router);
+require('./routes')(app);
+
+app.use(express.static(path.join(__dirname, '/template/shop')));
+
 
 app.use(function(err,req,res, next) {
 	//Node_ENV = 'production';
-	if(app.get('env') == 'development') {
-		var errorHandler = express.errorHandler();
-		errorHandler(err,req,res, next);
+	if(typeof err == 'number') {
+		err = new HttpError(err);
+	}
+	if(err instanceof HttpError) {
+		res.sendHttpError(err);
 	}
 	else {
-		res.send(500);
+		if(app.get('env') == 'development') {
+			express.errorHandler(err,req,res, next);		
+		}
+		else {
+			log.error(err);
+			err = new HttpError(500);
+			res.sendHttpError(err);
+		}
 	}
 });
 
-
-http.createServer(app).listen(config.get('port'), function(){
+var server = http.createServer(app);
+server.listen(config.get('port'), function(){
   console.log('Express server listening on port ' + config.get('port'));
 });
 
-/*var routes = require('./routes');
-var user = require('./routes/user');
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-
-
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-*/
+var io = require('./socket/index')(server);
+app.set('io', io);
--- createDB.js
+++ createDB.js
@@ -1,39 +1,53 @@
-var User = require('./models/user').User;
 var mongoose = require('./libs/mongoose');
 var async = require('async');
 
-
-mongoose.connection.on('open', function() {
-	var db = mongoose.connection.db;
-	db.dropDatabase(function(err){
-		if(err) throw err;
-		
-		async.parallel([
-
-			function(callback) {
-				var vasya = new User({username: 'Вася'})
-				vasya.save(function(err) {
-					callback(err, vasya);
-				});
-			},
-			function(callback) {
-				var petya = new User({username: 'Петя'})
-				petya.save(function(err) {
-					callback(err, petya);
-				});
-			},
-			function(callback) {
-				var admin = new User({username: 'admin'})
-				admin.save(function(err) {
-					callback(err, admin);
-				});
-			}
-		], function(err, results){
-			console.log(arguments);
-			mongoose.disconnect();
-		})
-
-	}); 
+async.series([
+	open,
+	dropDatabase,
+	requireModels,
+	createUsers,
+	close
+], function(err, results) {
+	console.log(arguments);
+	mongoose.disconnect();
+	process.exit(err ? 255 : 0);
 });
+
+function open(callback) {
+	mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+	var db = mongoose.connection.db;
+	db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+	require('./models/user')
+	async.each(Object.keys(mongoose.models), function(modelName, callback) {
+		mongoose.models[modelName].ensureIndexes(callback);
+	}, callback);
+
+}
+
+function createUsers(callback) {
+	
+	var users = [
+		{username: 'Вася'},
+		{username: 'Петя'},
+		{username: 'admin'}
+	];
+
+	async.each(users, function(userdata, callback) {
+		var user = new mongoose.models.User(userdata);
+		user.save(callback)
+	}, callback);
+	
+}
+
+function close(callback) {
+	
+}
+
 
 
